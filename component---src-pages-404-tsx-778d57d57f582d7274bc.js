1c91fb83281f6bd0243f633b14e561b7dd6bfdd6
amaurygomes <amaury_mju@hotmail.com>
2024-07-08 20:20:29 -0400

    Updates

---
+++ component---src-pages-404-tsx-778d57d57f582d7274bc.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportifolio=self.webpackChunkportifolio||[]).push([[218],{2513:function(e,t,n){n.r(t),n.d(t,{Head:function(){return o}});var l=n(7294),a=n(7015);t.default=()=>l.createElement(a.e,null,l.createElement("main",{className:"flex mx-auto"},l.createElement("h1",null,"Página não encontrada")));const o=()=>l.createElement("title",null,"Not found")}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-778d57d57f582d7274bc.js.map
